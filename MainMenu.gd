fccd6dd4e872c208cb4ee792c4ec01696c983d15
Miranda van der Ende <miranda.vdende@gmail.com>
2023-01-07 17:02:33 +0100

    Credits scene

--- MainMenu.gd
+++ MainMenu.gd
@@ -5,3 +5,7 @@ export var mainGameScene : PackedScene
 
 func _on_New_Game_Button_button_up():
 	get_tree().change_scene(mainGameScene.resource_path)
+
+
+func _on_Credits_Button_button_up():
+	get_tree().change_scene("res://Credits.tscn")
