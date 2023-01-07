da01b9f02347124d50d331a83338f3be8ba08c38
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-07 22:25:13 +0100

    make sure frame doesn't change in editor

--- MainMenu.gd
+++ MainMenu.gd
@@ -3,7 +3,7 @@ extends Node2D
 export var mainGameScene : PackedScene
 
 func _ready():
-	pass
+	$AnimatedSprite.play()
 
 func _on_New_Game_Button_button_up():
 	get_tree().change_scene(mainGameScene.resource_path)
