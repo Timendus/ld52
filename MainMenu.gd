0fc6617e0eea255fe77387ebc7e66fd70c6d0c55
Timendus <mail@timendus.com>
2023-01-07 20:24:26 +0100

    Add Highscores page and fix 2D scaling

--- MainMenu.gd
+++ MainMenu.gd
@@ -10,6 +10,8 @@ func _on_New_Game_Button_button_up():
 	#MusicController.play_track1()
 	MusicController.play_track2()
 
-
 func _on_Credits_Button_button_up():
 	get_tree().change_scene("res://Credits.tscn")
+
+func _on_High_Scores_Button_button_up():
+	get_tree().change_scene("res://HighScores.tscn")
