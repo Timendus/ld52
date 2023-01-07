cc87c1807600a23507b96c32e9ddd0dc0d0e1e8c
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-07 12:25:28 +0100

    init godot

--- devenv.nix
+++ devenv.nix
@@ -2,32 +2,6 @@
 
 {
   packages = with pkgs; [
-    python3
-    poetry
+    godot
   ];
-
-  env.LD_LIBRARY_PATH = with pkgs;
-    lib.makeLibraryPath ([
-      SDL2
-      SDL2_gfx
-      SDL2_image
-      SDL2_mixer
-      SDL2_ttf
-    ] ++ (lib.optionals pkgs.stdenv.isLinux [
-      xorg.libX11
-      xorg.libICE
-      xorg.libXi
-      xorg.libXScrnSaver
-      xorg.libXcursor
-      xorg.libXinerama
-      xorg.libXext
-      xorg.libXrandr
-      xorg.libXxf86vm
-    ]));
-
-  enterShell = ''
-    python -m venv ${config.env.DEVENV_STATE}/venv
-    source ${config.env.DEVENV_STATE}/venv/bin/activate
-    poetry install
-  '';
 }
