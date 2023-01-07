e8efa290c93e974b2ad3fe423040ef562aae8565
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-07 20:11:37 +0100

    player movement textures

--- Player.gd
+++ Player.gd
@@ -1,14 +1,34 @@
 extends RigidBody
 
+export var strafeSpeed = 2
+export var driftFactor = 100
+export var forwardTexture: Texture
+export var slideRightTexture: Texture
+export var slideLeftTexture: Texture
+var previousDirection = null
+
 func _ready():
 	pass # Replace with function body.
 
 
 func _process(delta):
-	if Input.is_action_pressed("forward"):
-		apply_central_impulse(global_transform.basis.x * 100 * delta)
+	var currentDirection = get_parent().transform.basis.x
 	var steering = (1 if Input.is_action_pressed("right") else 0) - (1 if Input.is_action_pressed("left") else 0)
-	apply_torque_impulse(Vector3(0, -1, 0) * steering * 0.01)
+	var drift = currentDirection.dot(previousDirection.rotated(Vector3.UP, PI/2)) if previousDirection != null else 0
+	var slide = strafeSpeed * steering + drift * driftFactor
+	get_node(".").translate_object_local(Vector3.FORWARD * delta * slide)
+	
+	previousDirection = currentDirection
+	
+	var texture = forwardTexture
+	if steering > 0:
+		texture = slideRightTexture
+	elif steering < 0:
+		texture = slideLeftTexture
+		
+	
+	get_node("MeshInstance").get_surface_material(0).albedo_texture = texture
+	
 
 func _process_physics(delta):
 	pass
