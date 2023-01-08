24483986dceb7395354ef4e1096598ec72af4b18
Timendus <mail@timendus.com>
2023-01-08 18:23:05 +0100

    Add more soundeffects

--- autoloads/MusicController.gd
+++ autoloads/MusicController.gd
@@ -1,12 +1,21 @@
 extends Node
 
+# Music files
 var menu_music = load("res://music/menu.mp3")
 var track1_leader_music = load("res://music/track1-leader.mp3")
 var track1_music = load("res://music/track1.mp3")
 var track2_leader_music = load("res://music/track2-leader.mp3")
 var track2_music = load("res://music/track2.mp3")
 
+# Sound effect files
+var chicken_sfx = load("res://assets/soundeffects/chicken.mp3")
+var crash_sfx = load("res://assets/soundeffects/crash.mp3")
+var level_sfx = load("res://assets/soundeffects/level.mp3")
+var pickup_sfx = load("res://assets/soundeffects/pickup.mp3")
+
+# Internal state and consts
 var playing
+
 const NOTHING = -1
 const MENU = 0
 const TRACK1 = 1
@@ -15,6 +24,7 @@ const TRACK2 = 2
 const CHICKEN = 1
 const CRASH = 2
 const PICKUP = 3
+const LEVEL = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,6 +34,8 @@ func _ready():
 	track1_music.loop = true
 	track2_music.loop = true
 	playing = NOTHING
+
+# Music stuff
 
 func play_menu():
 	if notYetPlaying(MENU):
@@ -42,21 +54,29 @@ func play_track2():
 		$MusicLoop.stream = track2_music
 		$MusicLeader.play()
 
-func playSFX(fx):
-	if fx == CHICKEN:
-		$Chicken.play()
-	if fx == CRASH:
-		$Crash.play()
-	if fx == PICKUP:
-		$Pickup.play()
+func notYetPlaying(song):
+	if playing != song:
+		playing = song
+		return true
+	return false
 
 # Callback handler when a song finishes playing (not counting looping songs)
 # Used to start the real song after the leader finishes
 func _on_Music_finished():
 	$MusicLoop.play()
 
-func notYetPlaying(song):
-	if playing != song:
-		playing = song
-		return true
-	return false
+# Sound effects stuff
+
+func playSFX(fx):
+	var effect = AudioStreamPlayer.new()
+	match fx:
+		CHICKEN: effect.stream = chicken_sfx
+		CRASH:   effect.stream = crash_sfx
+		PICKUP:  effect.stream = pickup_sfx
+		LEVEL:   effect.stream = level_sfx
+	add_child(effect)
+	effect.play()
+	effect.connect("finished", self, "_remove_Player", [effect])
+
+func _remove_Player(effect):
+	remove_child(effect)
