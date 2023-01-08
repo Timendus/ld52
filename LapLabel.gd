36b85e433a571b277da4a7b77a41a7efaba24029
Miranda van der Ende <miranda.vdende@gmail.com>
2023-01-08 16:12:44 +0100

    LapLabel is working

--- LapLabel.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-var current_lap = 0
-
-func update_score_label(score):
-	text = "Lap: %s" % current_lap
--- PathFollow.gd
+++ PathFollow.gd
@@ -6,6 +6,11 @@ export var speed = 0
 export var maxLaps = 3
 var laps = 0
 
+onready var label = get_tree().get_root().find_node("LapLabel", true, false)
+
+func _ready():
+	print(label)
+
 func _process(delta):
 	speed = min(speed + acceleration * delta, maxSpeed)
 	var oldOffset = offset
@@ -13,8 +18,8 @@ func _process(delta):
 	if sign(offset - oldOffset) != sign(speed):
 		# Offset got wrapped by Godot. It passed 1, so it was reset to 0,
 		# or the other way around.
-		# TODO: Make sure this updates the lap counter
 		laps += 1
+		label.text = "Lap: %s/3" % str(laps)
 		if laps > maxLaps: 
 			$Player.emit_signal("finished_level")
 	
