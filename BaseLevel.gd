bbd0e1bb87e19edbd0204899027fcca9f36ed485
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-08 16:43:26 +0100

    added finish line

---
+++ BaseLevel.gd
@@ -0,0 +1,14 @@
+extends WorldEnvironment
+
+export var finishScene: PackedScene
+
+func _ready():
+	var curve = $Path.curve
+	var start = curve.interpolate_baked(0, true)
+	var forward: Vector3 = (curve.interpolate_baked(0.01, true) - start).normalized()
+	var up = forward.rotated(Vector3.RIGHT, PI/2)
+	var right = forward.rotated(Vector3.UP, PI/2)
+	var finishNode = finishScene.instance()
+	finishNode.set_transform(Transform(forward, up, right, start))
+	add_child(finishNode)
+	
