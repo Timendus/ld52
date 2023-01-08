d28f1ca0a03b11a810b83020ef16310d83a7000b
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2023-01-08 19:31:49 +0100

    Score label not to 0 after levels

--- ScoreLabel.gd
+++ ScoreLabel.gd
@@ -2,6 +2,7 @@ extends Label
 
 func _ready():
 	ScoreController.connect("score_changed", self, "update_score_label")
+	text = "Score: %s" % ScoreController.current_score
 
 func update_score_label(score):
 	text = "Score: %s" % score
